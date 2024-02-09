1f2a2fdbdd1f78b17f2da0b5531ff1087d4d7a5e
koheinthurako <captainthura99@gmail.com>
2024-02-09 22:59:57 +0630

    add lombok annotation

--- src/main/java/com/validation/ValidationDemo/Entities/SavingAccount.java
+++ src/main/java/com/validation/ValidationDemo/Entities/SavingAccount.java
@@ -4,10 +4,20 @@ import java.time.LocalDate;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class SavingAccount {
 
 	private int a_id;
